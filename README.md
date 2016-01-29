87d49f126dd74f34514fb2b0b61286e1d883aebe
Herve <lijianyou1992>
2016-01-29 14:53:45 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+#AndroidVideoPlayer
+###Android Video Player , Like NetEaseNews Video Player.
+>Only Support mp4 & 3gp format  
+>Support play at partial view & fullscreen
+
+###Update Logs
+*1、Add DLNA Support*  
+*2、Fix bugs*
+
+
+`Screenshots` 
+##NetEaseNews Player
+![VideoPlayer_1](http://7vzsca.com1.z0.glb.clouddn.com/Screenshot_2015-08-04-17-10-05.png_img400w)
+##Play At Part
+![VideoPlayer_3](https://raw.githubusercontent.com/xiongwei-git/AndroidVideoPlayer/master/img/Screenshot_20151028-195103.png)
+##Play Fullscreen
+![VideoPlayer_4](https://raw.githubusercontent.com/xiongwei-git/AndroidVideoPlayer/master/img/Screenshot_20151028-195113.png)
---
+++ app/src/main/java/com/android/tedcoder/androidvideoplayer/MainActivity.java
@@ -0,0 +1,169 @@
+package com.android.tedcoder.androidvideoplayer;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.android.tedcoder.wkvideoplayer.dlna.engine.DLNAContainer;
+import com.android.tedcoder.wkvideoplayer.dlna.service.DLNAService;
+import com.android.tedcoder.wkvideoplayer.model.Video;
+import com.android.tedcoder.wkvideoplayer.model.VideoUrl;
+import com.android.tedcoder.wkvideoplayer.util.DensityUtil;
+import com.android.tedcoder.wkvideoplayer.view.MediaController;
+import com.android.tedcoder.wkvideoplayer.view.SuperVideoPlayer;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SuperVideoPlayer mSuperVideoPlayer;
+    private View mPlayBtnView;
+
+    private SuperVideoPlayer.VideoPlayCallbackImpl mVideoPlayCallback = new SuperVideoPlayer.VideoPlayCallbackImpl() {
+        @Override
+        public void onCloseVideo() {
+            mSuperVideoPlayer.close();
+            mPlayBtnView.setVisibility(View.VISIBLE);
+            mSuperVideoPlayer.setVisibility(View.GONE);
+            resetPageToPortrait();
+        }
+
+        @Override
+        public void onSwitchPageType() {
+            if (getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+                mSuperVideoPlayer.setPageType(MediaController.PageType.SHRINK);
+            } else {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+                mSuperVideoPlayer.setPageType(MediaController.PageType.EXPAND);
+            }
+        }
+
+        @Override
+        public void onPlayFinish() {
+
+        }
+    };
+
+    @Override
+    public void onClick(View view) {
+        mPlayBtnView.setVisibility(View.GONE);
+        mSuperVideoPlayer.setVisibility(View.VISIBLE);
+        mSuperVideoPlayer.setAutoHideController(false);
+
+        String url="http://video.uning.tv/4523da0c-9ae5-4702-8736-a05f43e2ca36/film/ac52b21e-60ce-48a6-83e8-b0cd31834a02.mp4?s=13373435";
+        Video video = new Video();
+        VideoUrl videoUrl1 = new VideoUrl();
+        videoUrl1.setFormatName("720P");
+//        videoUrl1.setFormatUrl("http://7xkbzx.com1.z0.glb.clouddn.com/SampleVideo_1080x720_20mb.mp4");
+        videoUrl1.setFormatUrl(url);
+        VideoUrl videoUrl2 = new VideoUrl();
+        videoUrl2.setFormatName("480P");
+//        videoUrl2.setFormatUrl("http://7xkbzx.com1.z0.glb.clouddn.com/SampleVideo_720x480_20mb.mp4");
+        videoUrl1.setFormatUrl(url);
+
+        ArrayList<VideoUrl> arrayList1 = new ArrayList<>();
+        arrayList1.add(videoUrl1);
+        arrayList1.add(videoUrl2);
+        video.setVideoName("测试视频一");
+        video.setVideoUrl(arrayList1);
+        Video video2 = new Video();
+        VideoUrl videoUrl3 = new VideoUrl();
+        videoUrl3.setFormatName("720P");
+//        videoUrl3.setFormatUrl("http://7xkbzx.com1.z0.glb.clouddn.com/SampleVideo_1080x720_10mb.mp4");
+        videoUrl1.setFormatUrl(url);
+
+        VideoUrl videoUrl4 = new VideoUrl();
+        videoUrl4.setFormatName("480P");
+//        videoUrl4.setFormatUrl("http://7xkbzx.com1.z0.glb.clouddn.com/SampleVideo_720x480_10mb.mp4");
+        videoUrl1.setFormatUrl(url);
+
+        ArrayList<VideoUrl> arrayList2 = new ArrayList<>();
+        arrayList2.add(videoUrl3);
+        arrayList2.add(videoUrl4);
+        video2.setVideoName("测试视频二");
+        video2.setVideoUrl(arrayList2);
+
+        ArrayList<Video> videoArrayList = new ArrayList<>();
+        videoArrayList.add(video);
+        videoArrayList.add(video2);
+
+        mSuperVideoPlayer.loadMultipleVideo(videoArrayList,0,0,0);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stopDLNAService();
+    }
+
+    /***
+     * 旋转屏幕之后回调
+     *
+     * @param newConfig newConfig
+     */
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (null == mSuperVideoPlayer) return;
+        /***
+         * 根据屏幕方向重新设置播放器的大小
+         */
+        if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().getDecorView().invalidate();
+            float height = DensityUtil.getWidthInPx(this);
+            float width = DensityUtil.getHeightInPx(this);
+            mSuperVideoPlayer.getLayoutParams().height = (int) width;
+            mSuperVideoPlayer.getLayoutParams().width = (int) height;
+        } else if (this.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            final WindowManager.LayoutParams attrs = getWindow().getAttributes();
+            attrs.flags &= (~WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().setAttributes(attrs);
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            float width = DensityUtil.getWidthInPx(this);
+            float height = DensityUtil.dip2px(this, 200.f);
+            mSuperVideoPlayer.getLayoutParams().height = (int) height;
+            mSuperVideoPlayer.getLayoutParams().width = (int) width;
+        }
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mSuperVideoPlayer = (SuperVideoPlayer) findViewById(R.id.video_player_item_1);
+        mPlayBtnView = findViewById(R.id.play_btn);
+        mPlayBtnView.setOnClickListener(this);
+        mSuperVideoPlayer.setVideoPlayCallback(mVideoPlayCallback);
+        startDLNAService();
+    }
+
+    /***
+     * 恢复屏幕至竖屏
+     */
+    private void resetPageToPortrait() {
+        if (getRequestedOrientation() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+            mSuperVideoPlayer.setPageType(MediaController.PageType.SHRINK);
+        }
+    }
+
+    private void startDLNAService() {
+        // Clear the device container.
+        DLNAContainer.getInstance().clear();
+        Intent intent = new Intent(getApplicationContext(), DLNAService.class);
+        startService(intent);
+    }
+
+    private void stopDLNAService() {
+        Intent intent = new Intent(getApplicationContext(), DLNAService.class);
+        stopService(intent);
+    }
+}
